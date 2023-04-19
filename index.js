e00808e88792933d63fe43e3540d91514f2ccdb2
khkang <alkalisummer@gmail.com>
2023-04-19 23:45:33 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const calc = require("./calc");
+const randomColor = require("randomcolor");
+console.log("덧셈결과: " + calc.add(2, 2));
+console.log("뺄셈결과: " + calc.sub(2, 2));
+console.log(calc.moduleName);
+console.log(randomColor());
